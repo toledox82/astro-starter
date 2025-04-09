0fd551b7c641b2d398ff4c2f241730a42ffadc5d
Marcio Toledo <toledox82@gmail.com>
2025-04-08 23:21:18 -0300

    Initial files

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://',
+  trailingSlash: 'always',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [
+    alpinejs(),
+    sitemap({
+      filter: (page) =>
+        !page.includes('404')
+    }),
+  ]
+});
