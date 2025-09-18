c76fafabb982de1a8750033c34cb6dd68b4f84b0
Marcio Toledo <toledox82@gmail.com>
2025-09-18 20:55:56 -0300

    Minimal done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap'
+import lenis from 'astro-lenis'
+
 import alpinejs from '@astrojs/alpinejs';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://toledointeractive.com',
-  trailingSlash: 'always',
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [
-    alpinejs(),
-    sitemap({
-      filter: (page) =>
-        !page.includes('404')
-    }),
-  ]
-});
+    site: 'https://bootcampiaparanegocios.com.br',
+    trailingSlash: 'never',
+    server: {
+        host: false, // faz bind em 0.0.0.0 para funcionar no WSL
+        port: 3000, // porta padrão que você quer
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [sitemap({
+        filter: (page) => !page.includes('404'),
+		}), lenis(), alpinejs()],
+})
